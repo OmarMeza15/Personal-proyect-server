7c97b0173025d91ae21a1ee0065d042bd54aff2b
Omar Meza <omarmezajarquin@gmail.com>
2023-03-03 22:18:55 +0100

    added route for the files

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
 
 // Configuration 
 const __filename = fileURLToPath(import.meta.url);
@@ -34,6 +35,9 @@ const storage = multer.diskStorage({
 })
 const upload = multer({ storage });
 
+// Routes with files
+app.post("/auth/register", upload.single("picture"), register);
+
 // Mongoose setup
 const PORT  = process.env.PORT || 6001;
 mongoose
@@ -45,3 +49,5 @@ mongoose
         app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
     })
     .catch((error) => console.log(`${error} did not connect`));
+
+
